5e4d794e6fa7f1e17d9bf47e61c8b28c805300fe
zhengjunjie <571088134@qq.com>
2018-06-05 18:31:50 +0800

    新增流式（stream）数据处理

--- src/test/java/cn/zjj/tips/base/controller/java8newspec/bean/Student.java
+++ src/test/java/cn/zjj/tips/base/controller/java8newspec/bean/Student.java
@@ -23,6 +23,15 @@ public class Student {
     /** 学校 */
     private String school;
 
+    public Student(long id, String name, int age, int grade, String major, String school) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.major = major;
+        this.school = school;
+    }
+
     public long getId() {
         return id;
     }
